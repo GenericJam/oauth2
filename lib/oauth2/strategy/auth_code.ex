d2ebe9a0130801739ce9b6e21baf032558750abc
GenericJam <genericjam@gmail.com>
2023-02-13 14:42:05 -0700

    Exclude client_id from token request 4

--- lib/oauth2/strategy/auth_code.ex
+++ lib/oauth2/strategy/auth_code.ex
@@ -53,9 +53,28 @@ defmodule OAuth2.Strategy.AuthCode do
     client
     |> put_param(:code, code)
     |> put_param(:grant_type, "authorization_code")
-    # |> put_param(:client_id, client.client_id)
+    |> put_param(:client_id, client.client_id)
     |> put_param(:redirect_uri, client.redirect_uri)
     |> merge_params(params)
+    |> basic_auth()
+    |> put_headers(headers)
+  end
+
+  @doc """
+  Retrieve an access token given the specified validation code.
+  """
+  @impl true
+  def get_okta_token(client, params, headers) do
+    {code, params} = Keyword.pop(params, :code, client.params["code"])
+
+    unless code do
+      raise OAuth2.Error, reason: "Missing required key `code` for `#{inspect(__MODULE__)}`"
+    end
+
+    client
+    |> put_param(:code, code)
+    |> put_param(:grant_type, "authorization_code")
+    |> put_param(:redirect_uri, client.redirect_uri)
     |> basic_auth()
     |> put_headers(headers)
   end
